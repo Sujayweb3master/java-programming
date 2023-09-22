f263659bf1d9d391510ac2988277fa99bd5136d9
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-09-22 21:11:56 +0530

    Add exercise folder which contains practice code

---
+++ exercises/calculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class calculator {
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int operation = sc.nextInt();
+
+        switch (operation) {
+            case 1: System.out.println(a + b);
+                    break;
+            case 2: System.out.println(a - b);
+                    break;
+            case 3: System.out.println(a * b);
+                    break;
+            case 4: System.out.println(a / b);
+                    break;
+            case 5: System.out.println(a % b);
+                    break;
+            default: System.out.println("Invalid input");
+        }
+    }
+}
