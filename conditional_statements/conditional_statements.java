bccce3d9409a1fdee1a30ebd6741740e2d252dda
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-09-22 20:25:47 +0530

    Add tables program, conditional_statements prg

---
+++ conditional_statements/conditional_statements.java
@@ -0,0 +1,13 @@
+class conditional_statements {
+    public static void main(String arg[]) {
+        int a = 10;
+        int b = 20;
+        
+        if(a==b) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
---
+++ cs/cs.java
@@ -0,0 +1,11 @@
+
+class cs {
+    public static void main(String arg[]) {
+        int a = 20;
+        int b = 20;
+        
+        if(a==b) {
+            System.out.println("true");
+        }
+    }
+}
