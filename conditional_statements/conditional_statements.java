67aaa3dcca679033b0ae67fe324e189da58db7fc
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-09-22 21:02:57 +0530

    Add example for switch conditional_statement

--- conditional_statements/conditional_statements.java
+++ conditional_statements/conditional_statements.java
@@ -3,24 +3,42 @@ import java.util.Scanner;
 class conditional_statements {
     public static void main(String arg[]) {
         
-        // identify even and odd number 
-        Scanner ss = new Scanner(System.in);
-        int n = ss.nextInt();
-
-        if (n % 2 == 0) {
-            System.out.println("Even number.");
-        } else {
-            System.out.println("Odd number.");
-        }
-
-        // age condition for voting
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
 
-        if(age < 10) {
-            System.out.println("Not elligible for voting.");
-        } else {
-            System.out.println("Elligible for voting.");
+        // using if and if else cs.
+        // identify even and odd number 
+       
+        // int n = sc.nextInt();
+
+        // if (n % 2 == 0) {
+        //     System.out.println("Even number.");
+        // } else {
+        //     System.out.println("Odd number.");
+        // }
+
+        // // age condition for voting
+        // // Scanner sc = new Scanner(System.in);
+        // int age = sc.nextInt();
+
+        // if(age < 10) {
+        //     System.out.println("Not elligible for voting.");
+        // } else {
+        //     System.out.println("Elligible for voting.");
+        // }
+
+        //using switch conditional statement.
+        int button = sc.nextInt();
+        switch (button) {
+            case 1: System.out.println("Hello");
+                    break;
+
+            case 2: System.out.println("Namaste!!!!");
+                    break;
+
+            case 3: System.out.println("Bonjour");
+                    break;
+
+            default: System.out.println("Invalid input.");
         }
     }
 }
