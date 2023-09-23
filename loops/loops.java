fd16cdde813983291a572f12a0a16403ab0c3ee4
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-09-23 12:13:52 +0530

    Add example code for loop statements

---
+++ loops/loops.java
@@ -0,0 +1,9 @@
+class loops {
+    public static void main(String arg[]) {
+        // for loop, while loop, do while loop
+
+        for(int counter = 0; counter<10; counter++) {
+            System.out.println("I am capable of anything !!!!!");
+        }
+    }
+}
