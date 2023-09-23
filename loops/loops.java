51e5ac0af85bc036d6abf0b0e25df315042a35f9
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-09-23 12:21:24 +0530

    Add example code for while loop

--- loops/loops.java
+++ loops/loops.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 class loops {
     public static void main(String arg[]) {
         // for loop, while loop, do while loop
 
-        for(int counter = 0; counter<10; counter++) {
+        //for loop
+        for(int counter = 0; counter<10; counter++) 
             System.out.println("I am capable of anything !!!!!");
+
+        //while loop
+        int i = 0;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(i < n) {
+            System.out.println(i);
+            i++;
         }
     }
 }
