8500aeb204f1a47bbe2d2dc85a60bbf2c67c758d
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-09-25 20:44:46 +0530

    Add another solution to exercise in lecture 4

--- exercises/menu.java
+++ exercises/menu.java
@@ -3,18 +3,41 @@ import java.util.Scanner;
 public class menu {
     public static void main (String arg[]) {
         
-        int marks;
+        int choice, marks;
 
-        System.out.println("Please enter 1 to proceed");
-        System.out.println("Press 0 to exit.");
+        // ------------Using switch CS----------------
+        // Scanner sc = new Scanner(System.in);
+        // int choice = sc.nextInt();
 
-        Scanner sc = new Scanner(System.in);
-        int choice = sc.nextInt();
+        // switch(choice) {
+        //     case 0: System.out.println("Exited!!");
+        //             break;
+            // case 1: System.out.println("Enter the marks: ");
+            //         marks = sc.nextInt();
+            //         if (marks >= 90)
+            //             System.out.println("This is good!!!");
+            //         else if ((89 >= marks)&&(marks >= 60))
+            //             System.out.println("This is also Good !!!");
+            //         else if ((59 >= marks) && (marks >= 0)) 
+            //             System.out.println("This is Good as well!!!");
+            //         else
+            //             System.out.println("Invalid marks!");
 
-        switch(choice) {
-            case 0: System.out.println("Exited!!");
-                    break;
-            case 1: System.out.println("Enter the marks: ");
+        //             break;
+        //     default: System.out.println("Invalid input"); 
+        // }
+
+        //---------Using do while loop -------
+        do {
+
+            System.out.println("Please enter 1 to proceed");
+            System.out.println("Press 0 to exit.");
+            
+            Scanner sc = new Scanner(System.in);
+            choice = sc.nextInt();
+
+            if (choice == 1){
+                System.out.println("Enter the marks: ");
                     marks = sc.nextInt();
                     if (marks >= 90)
                         System.out.println("This is good!!!");
@@ -25,9 +48,8 @@ public class menu {
                     else
                         System.out.println("Invalid marks!");
 
-                    break;
-            default: System.out.println("Invalid input"); 
-        }
+            }
+        } while ((choice != 0) && (choice == 1));
 
     }
 }
