8c0fb7f7d71df572f8ad0bdcfad32fb6a3a28d19
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-09-27 12:30:53 +0530

    Add simplified code for printing a pattern

--- patterns/patterns.java
+++ patterns/patterns.java
@@ -3,6 +3,9 @@ import java.util.*;
 public class patterns {
     public static void main (String arg[]) {
 
+        int n = 4; 
+        int m = 5;
+
         // solid rectangle (r=4, c=5)
         System.out.println("Solid Rectangle:");
         for (int i = 0; i < 4; i++) {
@@ -21,28 +24,43 @@ public class patterns {
             // *****
         System.out.println("Hollow Rectangle:");
 
-        for (int i = 1; i <= 4; i++) {
+        // for (int i = 1; i <= 4; i++) {
 
-            if ((i==1) || (i==4)) {
+        //     if ((i==1) || (i==4)) {
 
-                for (int j = 1; j < 5; j++) {
+        //         for (int j = 1; j < 5; j++) {
+        //             System.out.print("*");
+        //         }
+
+        //     } else {
+        //         for (int j = 1; j < 5; j++) {
+
+        //             if ((j==1) || (j==4)) 
+        //                 System.out.print("*");
+        //               else 
+        //                 System.out.print(" ");
+
+        //         }
+        //     }
+        //     System.out.println("");
+
+        // }
+
+        // Optimized and simplified code to print hollow pattern!!!
+        // outer loop 
+        for(int i = 1; i <= n; i++) {
+
+            //inner loop
+            for (int j = 1; j <= m; j++) {
+
+                if (i==1 || j==1 || i==n || j==m) {
                     System.out.print("*");
-                }
-
-            } else {
-                for (int j = 1; j < 5; j++) {
-
-                    if ((j==1) || (j==4)) 
-                        System.out.print("*");
-                      else 
-                        System.out.print(" ");
-
+                } else {
+                    System.out.print(" ");
                 }
             }
-            System.out.println("");
-
+            System.out.println();
         }
-
 
     }
 }
