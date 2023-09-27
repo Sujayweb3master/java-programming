ed6e3087d3461be3bfe8c23baa131ef5c35959b9
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-09-27 12:18:03 +0530

    Add more examples for pattern problems

--- patterns/patterns.java
+++ patterns/patterns.java
@@ -4,7 +4,7 @@ public class patterns {
     public static void main (String arg[]) {
 
         // solid rectangle (r=4, c=5)
-
+        System.out.println("Solid Rectangle:");
         for (int i = 0; i < 4; i++) {
 
             for (int j = 1; j < 5; j++) {
@@ -13,7 +13,36 @@ public class patterns {
             System.out.println("");
 
         }
+    
+        // hollow rectangle ()
+            // *****
+            // *   *
+            // *   *
+            // *****
+        System.out.println("Hollow Rectangle:");
 
-      
+        for (int i = 1; i <= 4; i++) {
+
+            if ((i==1) || (i==4)) {
+
+                for (int j = 1; j < 5; j++) {
+                    System.out.print("*");
+                }
+
+            } else {
+                for (int j = 1; j < 5; j++) {
+
+                    if ((j==1) || (j==4)) 
+                        System.out.print("*");
+                      else 
+                        System.out.print(" ");
+
+                }
+            }
+            System.out.println("");
+
+        }
+
+
     }
 }
