48afe5c72f3e6d5baf2dc1034431d52027287de0
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-09-27 12:03:23 +0530

    Add patterns folder to the existing codebase

---
+++ patterns/patterns.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class patterns {
+    public static void main (String arg[]) {
+
+        // solid rectangle (r=4, c=5)
+
+        for (int i = 0; i < 4; i++) {
+
+            for (int j = 1; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+
+        }
+
+      
+    }
+}
