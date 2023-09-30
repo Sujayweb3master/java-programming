4a0d1c17f076e82f6d78951833b34c6bbc461b4b
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-09-30 19:55:37 +0530

    Add solution to lecture5

--- exercises/lecture5/pattern.java
+++ exercises/lecture5/pattern.java
@@ -3,18 +3,38 @@ public class pattern {
 
         //1.  Solid Rhombus
         System.out.println("Solid Rhombus:");
-        int n = 40;
-        for(int i=1; i <= n; i++) {
-            
-            for(int j=1; j <= n-i; j++) {
+        int n1 = 5;
+        //outer loop
+        for(int i=1; i <= n1; i++) {
+            //inner loop 1
+            for(int j=1; j <= n1-i; j++) {
                 System.out.print(" ");
             }
-
-            for(int j=1; j <= n; j++) {
+            //inner loop2
+            for(int j=1; j <= n1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
+        //1.  Number pyramid
+        System.out.println("Number Pyramid:");
+        int n2 = 5;
+        //outer loop
+        for(int i=1; i <= n2; i++) {
+            //inner loop 1
+            for(int j=1; j <= n2-i; j++) {
+                System.out.print(" ");
+            }
+            //inner loop2
+            for(int j=1; j < (2 * i) ; j++) {
+                if(j % 2 != 0) {
+                    System.out.print(i);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
